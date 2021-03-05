fa85ca30961e7fcdfc5bd07eb4c499a9d00baa54
Connie Panzariello <connie.panzariello@gmail.com>
2021-03-04 22:35:01 -0500

    2nd commit, to grunt deploy

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,7 +2,6 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-jjj
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
